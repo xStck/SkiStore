6541bd2b13118445979cbeec283ca9a377ec5452
Dawid <dawidn392@gmail.com>
2024-05-17 20:45:43 +0200

    Client: added product listing + navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Product} from "./models/product";
+import {Pagination} from "./models/pagination";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'SkiStore';
+export class AppComponent implements OnInit {
+    title = 'SkiStore';
+    products: Product[] = [];
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit(): void {
+        this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+            next: response => this.products = response.data,
+            error: error => console.log(error),
+            complete: () => console.log('request completed')
+        })
+    }
 }
